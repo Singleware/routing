f4f99b3ddda8766b390054219c1ad8abe3c1942b
Silas B. Domingos <contato@balmante.eti.br>
2019-03-13 18:55:27 -0300

    Structural changes.

--- release/types/index.d.ts
+++ release/types/index.d.ts
@@ -1,7 +1,3 @@
-/**
- * Copyright (C) 2018 Silas B. Domingos
- * This source code is licensed under the MIT License as described in the file LICENSE.
- */
 export { Route } from './route';
 export { Constraint } from './constraint';
 export { Variables } from './variables';
